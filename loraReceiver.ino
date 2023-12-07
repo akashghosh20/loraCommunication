6db9c555bc7fab2a3e00db623d91a23275e7c5ed
Akash Ghosh <96739678+akashghosh20@users.noreply.github.com>
2023-12-07 22:26:51 +0600

    Update loraReceiver.ino

--- loraReceiver.ino
+++ loraReceiver.ino
@@ -1,27 +1,29 @@
 #include <RH_RF95.h>
+#include <SPI.h>
 
-RH_RF95 rf95;
+#define SX1278_CS 5 // Replace with the GPIO pin number you connected to NSS (CS)
+#define RECEIVER_ADDRESS 1 // Change this address to match the sender's address
 
-const uint8_t receiverAddress = 1; // Change this address for each receiver
+RH_RF95 rf95(SX1278_CS);
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial);
+
   if (!rf95.init()) {
     Serial.println("LoRa initialization failed");
     while (1);
   }
 
-  if (!rf95.setThisAddress(receiverAddress)) {
-    Serial.println("Address setting failed");
-    while (1);
-  }
+  // Set the frequency to match your LoRa module (e.g., 433.0 for SX1278)
+  rf95.setFrequency(433.0);
 
-  rf95.setPromiscuous(false); // Disable promiscuous mode
+  rf95.setHeaderFrom(RECEIVER_ADDRESS); // Set the receiver's own address
 }
 
 void loop() {
   if (rf95.available()) {
-    // Should be a message for us now   
+    // Should be a message for us now
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
 
