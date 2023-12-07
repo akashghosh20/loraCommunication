12d03d452b2722717341a17aba66c8c0fc1ee3ce
Akash Ghosh <96739678+akashghosh20@users.noreply.github.com>
2023-12-07 22:50:00 +0600

    Update loraSender.ino

--- loraSender.ino
+++ loraSender.ino
@@ -1,32 +1,33 @@
-#include <RH_RF95.h>
 #include <SPI.h>
+#include <LoRa.h>
 
-#define SX1278_CS 5 // Replace with the GPIO pin number you connected to NSS (CS)
+#define SX1262_CS 5 // Replace with the GPIO pin number you connected to NSS (CS)
 #define RECEIVER_ADDRESS 1 // Change this address for the specific receiver
 
-RH_RF95 rf95(SX1278_CS);
+int sensorValuePin = 15;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  if (!rf95.init()) {
+  if (!LoRa.begin(SX1262_CS)) {
     Serial.println("LoRa initialization failed");
     while (1);
   }
 
-  // Set the frequency to match your LoRa module (e.g., 433.0 for SX1278)
-  rf95.setFrequency(433.0);
+  // Set the frequency to match your LoRa module (e.g., 868.0 for SX1262)
+  LoRa.setFrequency(868.0);
 }
 
 void loop() {
-  const char* message = "Hello from the sender";
+  int sensorValue = analogRead(sensorValuePin);
 
-  rf95.setHeaderTo(RECEIVER_ADDRESS); // Set the receiver address
-  rf95.send((uint8_t*)message, strlen(message));
-  rf95.waitPacketSent();
+  LoRa.beginPacket();
+  LoRa.setTxAddress(RECEIVER_ADDRESS); // Set the receiver address
+  LoRa.print(sensorValue);
+  LoRa.endPacket();
 
-  Serial.println("Sent: " + String(message));
+  Serial.println("Sent: " + String(sensorValue));
 
   delay(5000); // Send every 5 seconds
 }
